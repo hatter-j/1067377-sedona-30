77e1eb3c81444e5cec206a99274be58407365d4c
hatter-j <hatterjulia@mail.ru>
2021-03-23 11:10:54 +0300

    17. Оживляем интерактивные элементы

--- js/finder-form.js
+++ js/finder-form.js
@@ -12,27 +12,18 @@ let storageChildren = "";
 
 try {
   storageAdults = localStorage.getItem("adultsNumber");
-} catch (err) {
-  isStorageSupport = false;
-};
-
-try {
   storageChildren = localStorage.getItem("childrenNumber");
 } catch (err) {
   isStorageSupport = false;
 };
 
 if (isStorageSupport) {
-  adultsNumber.value = storageAdults;
-  childrenNumber.value = storageChildren;
-};
-
-if (storageAdults) {
-  adultsNumber.value = storageAdults;
-};
-
-if (storageChildren) {
-  childrenNumber.value = storageChildren;
+  if (storageAdults) {
+    adultsNumber.value = storageAdults;
+  };
+  if (storageChildren) {
+    childrenNumber.value = storageChildren;
+  };
 };
 
 finderButton.addEventListener("click", function (evt) {
@@ -40,36 +31,15 @@ finderButton.addEventListener("click", function (evt) {
 });
 
 searchForm.addEventListener("submit", function (evt) {
-  if (!arrivalDate.value || !departureDate.value) {
+  if (!arrivalDate.value || !departureDate.value || !adultsNumber.value || !childrenNumber.value) {
     evt.preventDefault();
     formContainer.classList.remove("form-container-error");
     formContainer.offsetWidth = formContainer.offsetWidth;
     formContainer.classList.add("form-container-error");
-}
-});
-
-searchForm.addEventListener("submit", function (evt) {
-  if (!adultsNumber.value) {
-    evt.preventDefault();
-    formContainer.classList.remove("form-container-error");
-    formContainer.offsetWidth = formContainer.offsetWidth;
-    formContainer.classList.add("form-container-error");
-} else {
-  if (isStorageSupport) {
-    localStorage.setItem("adultsNumber", adultsNumber.value);
-  }
-}
-});
-
-searchForm.addEventListener("submit", function (evt) {
-  if (!childrenNumber.value) {
-    evt.preventDefault();
-    formContainer.classList.remove("form-container-error");
-    formContainer.offsetWidth = formContainer.offsetWidth;
-    formContainer.classList.add("form-container-error");
-} else {
-  if (isStorageSupport) {
-    localStorage.setItem("childrenNumber", childrenNumber.value);
-  }
-}
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("adultsNumber", adultsNumber.value);
+      localStorage.setItem("childrenNumber", childrenNumber.value);
+    }
+    }
 });
